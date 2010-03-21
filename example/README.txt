09d10139815fbf16d17bf0715d9b523b45cb9cea
Michael Elsdoerfer <michael@elsdoerfer.com>
2010-03-21 13:45:58 +0100

    Added example project.

---
+++ example/README.txt
@@ -0,0 +1,3 @@
+To compile this, you need to configure a class path variable STACKTRACE_SRC_PATH
+pointing to the ./src directory of the path of the library, which is essentially
+"../src".
---
+++ example/src/com/nullwire/trace/example/MainActivity.java
@@ -0,0 +1,65 @@
+package com.nullwire.trace.example;
+
+import com.nullwire.trace.ExceptionHandler;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends Activity {
+
+	private static final int DIALOG_SUBMITTING_EXCEPTIONS = 1;
+
+	private Dialog mExceptionSubmitDialog;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        ExceptionHandler.setup(this, new ExceptionHandler.Processor() {
+        	@Override
+			public boolean beginSubmit() {
+				showDialog(DIALOG_SUBMITTING_EXCEPTIONS);
+				return true;
+			}
+
+			@Override
+			public void submitDone() {
+				mExceptionSubmitDialog.cancel();
+			}
+
+			@Override
+			public void handlerInstalled() {
+				continueActivitySetup();
+			}
+		});
+    }
+
+    private void continueActivitySetup() {
+    	findViewById(R.id.crash_button).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				throw new RuntimeException("exception requested by user");
+			}
+		});
+    }
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		if (id == DIALOG_SUBMITTING_EXCEPTIONS) {
+			mExceptionSubmitDialog = new AlertDialog.Builder(this)
+				.setTitle("Please wait...")
+				.setMessage("The application has previously crashed "+
+						"unexpectedly. Please wait while I am submitting "+
+						"some information that will help us fix the problem.")
+				.setCancelable(false).create();
+			return mExceptionSubmitDialog;
+		}
+		else
+			return super.onCreateDialog(id);
+	}
+}
